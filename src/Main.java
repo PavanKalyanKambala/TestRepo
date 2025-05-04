93ff456248df6de0b0ce456549d386949852a738
PavanKalyanKambala <pavan.wiu@gmail.com>
2025-05-04 00:31:52 -0500

    testing

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main
+{
+    int a=10;
+    static int b = 20;
+    public static void main(String[] args)
+    {
+        Main m = new Main();
+        m.yes();
+        System.out.println(m.a);
+        System.out.println(b);
+    }
+    public void yes(int... c)
+    {
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
